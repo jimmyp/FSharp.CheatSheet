5d9c1b8fa14bc045aeb20f40770953040616be7d
Jim Pelletier <jpelletier@liberty.com.au>
2019-03-25 09:30:30 +1100

    adds list map

--- Tests.fs
+++ Tests.fs
@@ -4,12 +4,14 @@ open System
 open Xunit
 
 let AssertSomeEquals : ('a * Option<'a>) -> unit =
-   fun (expected, actual) ->
-    match actual with
-    | Some x ->
-      if x = expected then ()
-      else (failwith (sprintf "expected %A but got %A" expected  x))
-    | None -> failwith (sprintf "expected Some but was None")
+    fun (expected, actual) ->
+      match actual with
+      | Some x ->
+        if x = expected then ()
+        else (failwith (sprintf "expected %A but got %A" expected  x))
+      | None -> failwith (sprintf "expected Some but was None")
+      
+      
 
 [<Fact>]
 let ``Option Map`` () =
@@ -26,3 +28,17 @@ let ``Option Map`` () =
     Assert.True (some1.IsSome)
     AssertSomeEquals (1, some1)
 
+[<Fact>]
+let ``List map`` () =
+
+  let rec map : ('a -> 'b) -> List<'a> -> List<'b> =
+    fun f a -> 
+      match a with
+      | x :: xs -> (f x) :: (xs |> map f)
+      | [] -> []
+
+  let strings = ["1"; "2"]
+  let integers = strings |> map (fun x -> Int32.Parse (x))
+
+  Assert.NotEmpty (integers)
+  Assert.Equal<List<int>> ([1; 2], integers)
